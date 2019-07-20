25a47d6638ecf3d8ea0f7c156d8884bbdb9a60aa
Jack Gay <jackpowis@gmail.com>
2019-07-20 16:50:23 +0100

    working on functionality

--- js/app.js
+++ js/app.js
@@ -11,40 +11,49 @@ class UI {
     this.expenseForm = document.getElementById("expense-form");
     this.expenseInput = document.getElementById("expense-input");
     this.amountInput = document.getElementById("amount-input");
-    this.expenseList = document.getElementById("expense-list"); 
+    this.expenseList = document.getElementById("expense-list");
     this.itemList = [];
     this.itemID = 0;
   }
 
+  //submit budget method
+  submitBudgetForm() {
+    const value = this.budgetInput.value;
+    if (value = '' || value < 0) {
+        this.budgetFeedback.classList.add('showItem')
+        this.budgetFeedback.innerHTML = `<p> value cannot be empty or negative </p>`
+    }
+  }
 }
 
 function eventListeners() {
   const budgetForm = document.getElementById('budget-form')
   const expenseForm = document.getElementById('expense-form')
-  const expenseList = document.getElementById('expense-list') 
+  const expenseList = document.getElementById('expense-list')
   //new instance of UI CLASS
   const ui = new UI()
 
   //budget form submit 
 
-  budgetForm.addEventListener('submit', function(e){
-
+  budgetForm.addEventListener('submit', function (e) {
+    event.preventDefault();
+    ui.submitBudgetForm();
   })
 
   //expense form submit
-  expenseForm.addEventListener('submit', function(e){
-    
+  expenseForm.addEventListener('submit', function (e) {
+    event.preventDefault();
   })
 
 
-  //expense list submit
-  expenseList.addEventListener('submit', function(e){
-    
+  //expense click
+  expenseList.addEventListener('click', function () {
+
   })
 
 
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   eventListeners();
 })
