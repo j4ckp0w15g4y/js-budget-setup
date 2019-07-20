ac7b44c8bfbc6f50d736a12c47b735fd06a0843a
Jack Gay <jackpowis@gmail.com>
2019-07-20 17:09:20 +0100

    functionality commit

--- js/app.js
+++ js/app.js
@@ -20,8 +20,15 @@ class UI {
   submitBudgetForm() {
     const value = this.budgetInput.value;
     if (value === '' || value < 0) {
-        this.budgetFeedback.classList.add('showItem')
-        this.budgetFeedback.innerHTML = `<p> value cannot be empty or negative </p>`
+        this.budgetFeedback.classList.add("showItem")
+        this.budgetFeedback.innerHTML = `<p> value cannot be empty or negative </p>`;
+        const self = this;
+        console.log(this);
+        setTimeout(function() {
+          console.log(this);
+            console.log(self);
+          this.budgetFeedback.classList.remove("showItem");                    
+        }, 4000);
     }
   }
 }
